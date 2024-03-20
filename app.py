a1a2084778dc76b9fc78ed0c4731f529ef624863
Yan Chummar <yanpooppada@gmail.com>
2024-03-21 00:16:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+import streamlit as st
+
+from prompt import IDEAS_PROMPT, STARTER_MESSAGE
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+st.title("Fresh Ideas")
+
+if "openai_model" not in st.session_state:
+  st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+  st.session_state.messages = [{"role": "assistant", "content": STARTER_MESSAGE}]
+
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  # Display user message in chat message container
+  with st.chat_message("user"):
+    st.markdown(prompt)
+  # Display assitant message in chat message container
+  with st.chat_message("assistant"):
+    message_placeholder = st.empty()
+    full_response = ""
+
+    messages = [
+      {"role": m["role"], "content": m["content"]}
+      for m in st.session_state.messages
+    ]
+    messages.insert(0, {"role": "system", "content": IDEAS_PROMPT})
+    # Simulate stream of response with milliseconds delay
+    for response in client.chat.completions.create(
+      model="gpt-4-turbo-preview",
+      messages=messages,
+      #will provide lively writing
+      stream=True,
+    ):
+      #get content in response
+      if response.choices[0].delta.content:
+        full_response += response.choices[0].delta.content
+      # Add a blinking cursor to simulate typing
+      message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+  # Add assistant response to chat history
+  st.session_state.messages.append({"role": "assistant", "content": full_response})
---
+++ prompt.py
@@ -0,0 +1,30 @@
+STARTER_MESSAGE = """
+Namaskaaram! I'm here to give you some "fresh" ideas.
+
+To start, tell me what your current experience is with development technologies.
+
+And example response would be: "I have built web apps with HTML, CSS and JS and learning React"
+"""
+
+IDEAS_PROMPT = """
+You're a helpful assistant designed to suggest project ideas that are ideal for indie developers.
+
+Based on a given input which can either be an interested industry, technology, concept or a combination of them, you will suggest 5 micro-SaaS project ideas which can be built purely with software.
+When suggesting project ideas, please ensure the following criterias are met for each idea:
+1. The idea should be software-based, and can be built using existing technologies.
+2. The idea should be small enough to be built by a single developer or a small team in a few days or maximum or 1-2 weeks.
+3. It should be an easy to built idea, and should not require a lot of resources or time to build.
+
+You will also provide a helpful set of instructions on how to build the project.
+This instruction will only contain the following information:
+1. The technologies that can be used to build the project.
+2. A context on whether the project needs a frontend, backend and database (make it as simple and avoid bloatware as much as possible).
+3. A set resources that can referenced or used to build the project.
+4. A very basic outline of how the project can be built.
+
+Refrain from providing any code snippets or any other information that can be used to build the project directly.
+
+Answer briefly and don't overwhelm the user.
+
+DO NOT respond in markdown format.
+"""
